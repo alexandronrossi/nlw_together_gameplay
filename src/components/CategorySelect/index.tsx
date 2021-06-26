e3737f2c9b2e8b0cbfa8169fee54a7109218bdec
Alexandro do Nascimento Rossi <alexandro@wapstore.com.br>
2021-06-26 17:39:16 -0300

    Dia 2 - Parte 1/2

---
+++ src/components/CategorySelect/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { ScrollView } from 'react-native';
+import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
+
+import { styles } from './styles';
+import { categories } from '../../utils/categories';
+import { Category } from '../Category';
+
+type Prop = {
+  CategorySelected: string;
+  setCategory: (categoryId: string) => void;
+}
+
+export function CategorySelect({CategorySelected, setCategory} : Prop) {
+  return (
+    <ScrollView 
+      horizontal 
+      style={styles.container} 
+      showsHorizontalScrollIndicator={false} 
+      contentContainerStyle={{paddingRight: 40}} 
+    >
+      {
+        categories.map(category => (
+          <Category 
+            key={category.id} 
+            title={category.title} 
+            icon={category.icon} 
+            checked={category.id === CategorySelected} 
+            onPress={() => setCategory(category.id)}
+          />
+        ))
+      }
+    </ScrollView>
+  );
+}
